3daf6bb81b88f20cf388a03a13e1a7cc7bd55617
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-10-25 23:14:57 +0900

    #3 サンプル用のUsersAPI削除

--- cmd/server/router.go
+++ cmd/server/router.go
@@ -7,9 +7,6 @@ import (
 	_methodHandler "github.com/Hajime3778/api-creator-backend/pkg/apis/method/handler"
 	_methodRepository "github.com/Hajime3778/api-creator-backend/pkg/apis/method/repository"
 	_methodUsecase "github.com/Hajime3778/api-creator-backend/pkg/apis/method/usecase"
-	_userHandler "github.com/Hajime3778/api-creator-backend/pkg/apis/user/handler"
-	_userRepository "github.com/Hajime3778/api-creator-backend/pkg/apis/user/repository"
-	_userUsecase "github.com/Hajime3778/api-creator-backend/pkg/apis/user/usecase"
 
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
@@ -33,11 +30,6 @@ func newRouter() *gin.Engine {
 func (s *Server) SetUpRouter() *gin.Engine {
 	// Group v1
 	apiV1 := s.router.Group("api/v1")
-
-	// Users
-	userRepository := _userRepository.NewUserRepository(s.db)
-	userUsecase := _userUsecase.NewUserUsecase(userRepository)
-	_userHandler.NewUserHandler(apiV1, userUsecase)
 
 	// APIs
 	apiRepository := _apiRepository.NewAPIRepository(s.db)
