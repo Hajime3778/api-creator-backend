8aa26b05eadfd328b293ff22dbc209b5eb120633
Hajime Takeuchi <shadowthe8156@gmail.com>
2020-11-09 23:10:57 +0900

    #15 mongo用のconfig追加

--- app/api-creator-documents/api-creator-documents.go
+++ app/api-creator-documents/api-creator-documents.go
@@ -12,6 +12,17 @@ import (
 )
 
 func main() {
+	// logger.LoggingSetting("./log/")
+	// cfg := config.NewConfig("./documents.config.json")
+	// db := database.DB{
+	// 	Host:       cfg.DataBase.Host,
+	// 	Port:       cfg.DataBase.Port,
+	// 	Username:   cfg.DataBase.User,
+	// 	Password:   cfg.DataBase.Password,
+	// 	DBName:     cfg.DataBase.Database,
+	// 	Connection: cfg.DataBase.Host,
+	// }
+
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://root:example@localhost:27017"))
